f5e40a1861f60f95215b327f8ad79c5201592fee
Keinsaxophonist <96473684+Keinsaxophonist@users.noreply.github.com>
2022-02-11 17:46:38 +0100

    Add files via upload

--- substantive/substantive.js
+++ substantive/substantive.js
@@ -4,20 +4,40 @@ var tmp = document.getElementById("textfeld").value;
 
 
 //erster Durchgang
-    /*tmp = tmp.replaceAll(" ", "', <br/>'");
-    tmp = tmp.replaceAll(":", " ");
-    tmp = tmp.replaceAll(".", " ");
-	tmp = tmp.replaceAll("(", " ");
-	tmp = tmp.replaceAll(")", " ");
-	tmp = tmp.replaceAll('"', " ");*/
+    //tmp = tmp.replaceAll(" ", "', <br/>'");
+    tmp = tmp.replaceAll(" ", "###");
+	tmp = tmp.replaceAll(":", "###");
+    tmp = tmp.replaceAll(".", "###");
+	tmp = tmp.replaceAll("(", "###");
+	tmp = tmp.replaceAll(")", "###");
+	tmp = tmp.replaceAll('"', "###");
+	tmp = tmp.replaceAll(',', "###");
+	tmp = tmp.replaceAll('*', "###");
+	tmp = tmp.replaceAll('\n', "###");
+	tmp = tmp.replaceAll('…', "###");
+	tmp = tmp.replaceAll('?', "###");
+	tmp = tmp.replaceAll('!', "###");
+	tmp = tmp.replaceAll(';', "###");
+	
+	
+	for (var i = 0; i < 15; i++)
+	{
+		tmp = tmp.replaceAll("##", "#");
+	}		
+	
+	
+	tmp = tmp.split("#");
+	tmp = tmp.sort();
+	var erg = [...new Set(tmp)];
+
 	
 //zweiter Durchgang
 
-tmp = tmp.replaceAll(",", "', <br/>'");
+//tmp = tmp.replaceAll(",", "', <br/>'");
 	
 	
 	
 	
 	
-    document.getElementById("label").innerHTML ="var Worte = ['" +  tmp + " ']";
+    document.getElementById("label").innerHTML ="var Worte = ['" +  erg[0] + " ']";
 }
